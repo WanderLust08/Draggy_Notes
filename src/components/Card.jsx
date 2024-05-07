e5853d6b2dbb7694ce0421f9dc27f4d18f5c1d3c
Samarth Shetty <samarthshetty78.ss39@gmail.com>
2024-05-08 04:24:36 +0530

    Third Commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,11 +5,11 @@ import { IoClose } from "react-icons/io5";
 
 import { motion } from "framer-motion"
 
-const Card = ({data,reference}) => {
+const Card = ({data,refer}) => {
   return (
-    <motion.div drag dragConstraints={reference} whileDrag={{scale:1.2}} className="relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden ">
+    <motion.div drag dragConstraints={refer} whileDrag={{scale:1.2}} className="relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden ">
         <FaRegFileAlt />
-        <p className='text-sm leading-tight mt-5 font-semibold '>{data.desc}</p>
+        <p className='text-md leading-tight mt-5 font-semibold '>{data.desc}</p>
         <div className="footer absolute bottom-0 left-0 w-full">
             <div className="flex items-center justify-between py-3 px-8 mb-5">
                 <h5>{data.filesize}</h5>
@@ -22,7 +22,7 @@ const Card = ({data,reference}) => {
             {
             data.tag.isOpen && (  
                 <div className={`tag w-full py-4 bg-${data.tag.tagColor}-600 flex items-center justify-center`}>
-                <h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
+                <h3 className="text-lg font-semibold">{data.tag.tagTitle}</h3>
             </div>
             )}
 
--- src/components/Foregorund.jsx
+++ src/components/Foregorund.jsx
@@ -9,27 +9,27 @@ const Foregorund = () => {
       desc: "Hello, This is Dynamic Conditional Data rendering in ReactJs",
       filesize: ".9mb",
       close: false,
-      tag: { isOpen: true, tagTitle: "Download Now", tagColor:"blue"},
+      tag: { isOpen: true, tagTitle: "Upload", tagColor:"blue"},
     },
     {
-        desc: "Hello, This is Dynamic Conditional Data rendering in ReactJs",
+        desc: "Hello, I have got to improve this app for sure.",
         filesize: ".9mb",
         close: false,
-        tag: { isOpen: false, tagTitle: "Upload", tagColor:"green"},
+        tag: { isOpen: false, tagTitle: "Download Now", tagColor:"green"},
       },
       {
-        desc: "Hello, This is Dynamic Conditional Data rendering in ReactJs",
+        desc: "Hello, I Have not used any major hooks or props as of now.",
         filesize: ".9mb",
         close: false,
-        tag: { isOpen: true, tagTitle: "Download Now", tagColor:"purple"},
+        tag: { isOpen: true, tagTitle: "Commit", tagColor:"purple"},
       },
   ];
 
-  useState();
+  // useState();
   return (
     <div ref = {ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap p-5">
         {data.map((item,index)=>(
-          <Card data={item} reference = {ref}/>  
+          <Card data={item} refer = {ref}/>  
         ))}
     </div>
   );
